10d1ebe3556040695baf7c15eda270839e6822af
Yahav Dan <89801515+Yahavmdan@users.noreply.github.com>
2022-03-04 18:06:05 +0200

    Add files via upload

---
+++ real-app/my-rest-api/app.js
@@ -0,0 +1,30 @@
+require("dotenv/config");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const cards = require("./routes/cards");
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const mongoose = require("mongoose");
+const cors = require("cors");
+console.log(process.env.MONGO_DB);
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/cards", cards);
+
+const port = 3900;
+
+http.listen(port, () => console.log(`Listening on port ${port}...`));
